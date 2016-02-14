e66d0031cd7ba60b53bba7a9c8cbda2994a910f3
NehaIrfan <nehairfan5@gmail.com>
2016-02-14 17:43:44 +0530

    Menu screens added - Back button working

---
+++ app/src/main/java/com/example/nidatazeen/shoppingadviser1/ContactUsActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.nidatazeen.shoppingadviser1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ContactUsActivityFragment extends Fragment {
+
+    public ContactUsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/nidatazeen/shoppingadviser1/MyAccountsActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.nidatazeen.shoppingadviser1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MyAccountsActivityFragment extends Fragment {
+
+    public MyAccountsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_accounts, container, false);
+    }
+}
