a03bd38356b138f97a5b97032c2a5a1a523947c6
NehaIrfan <nehairfan5@gmail.com>
2016-02-14 21:25:28 +0530

    New changes - button to open email app

--- app/src/main/java/com/example/nidatazeen/shoppingadviser1/ContactUsActivity.java
+++ app/src/main/java/com/example/nidatazeen/shoppingadviser1/ContactUsActivity.java
@@ -1,12 +1,13 @@
 package com.example.nidatazeen.shoppingadviser1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
+import android.widget.Button;
 public class ContactUsActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +16,27 @@ public class ContactUsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contact_us);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button btnFoo = (Button) findViewById(R.id.btnFoo);
+        btnFoo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+
+/* Fill it with Data */
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"support@shoppingadviser.in"});
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject");
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Text");
+
+/* Send it off to the Activity-Chooser */
+                startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+//                Intent intent = new Intent();
+//                intent.setClass(ContactUsActivity.this, MyAccountsActivity.class);
+//                startActivity(intent);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
